6204543760a245ac59af80ceb210770cd5ad2a4f
magmom95 <magmom95@naver.com>
2024-07-20 19:24:37 +0900

    ing api

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,25 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [isLoading, setisLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovie = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovie(json);
+    setisLoading(false);
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
   return (
     <div>
-      <h1>Hello!</h1>
+      <h1>{isLoading ? "Loading" : JSON.stringify(movie)}</h1>
     </div>
   );
 }
