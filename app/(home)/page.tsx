eae27911a0532f64407377918e6e0ffba2943381
magmom95 <magmom95@naver.com>
2024-07-24 01:39:39 +0900

    ing

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata = {
   title: "Home",
@@ -17,7 +17,7 @@ async function getMovie() {
 export default async function HomePage() {
   const movies = await getMovie();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
         <Movie
           key={movie.id}
