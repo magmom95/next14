fd98e6059ecb5d955abac10b450c6d37f3877671
magmom95 <magmom95@naver.com>
2024-07-27 03:53:28 +0900

    finish

--- components/movie.tsx
+++ components/movie.tsx
@@ -18,6 +18,7 @@ export default function Movie({ title, id, poster_path }: IMovieProps) {
   return (
     <div className={styles.movie}>
       <img src={poster_path} alt={title} onClick={onClick} />
+      {/* <Link prefetch href={`/movies/${id}`}> 누르지 않아도 미리 자바스크립트통신해서 로딩 빠르게 즉 ssr처럼 행동하게함*/}
       <Link href={`/movies/${id}`}>{title}</Link>
     </div>
   );
